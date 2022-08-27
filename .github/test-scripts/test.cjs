ede1f53337671c87e0b630e020b110e313198935
AlCalzone <d.griesel@gmx.net>
2022-08-27 12:31:09 +0200

    feat: convert to hybrid package (#1)

---
+++ .github/test-scripts/test.cjs
@@ -0,0 +1,4 @@
+const pAny = require("@esm2cjs/p-any").default;
+const assert = require("assert");
+
+assert(typeof pAny === "function");
---
+++ .github/test-scripts/test.mjs
@@ -0,0 +1,4 @@
+import pAny from "@esm2cjs/p-any";
+import assert from "assert";
+
+assert(typeof pAny === "function");
